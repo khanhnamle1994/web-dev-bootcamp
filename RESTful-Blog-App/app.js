e9bfe444a97a87c54beb20031d290d7339e46a3f
James Le <le_j6@denison.edu>
2017-10-25 16:06:20 +0700

    RESTful Blog App: NEW and CREATE

--- RESTful-Blog-App/app.js
+++ RESTful-Blog-App/app.js
@@ -19,17 +19,35 @@ var blogSchema = new mongoose.Schema({
 var Blog = mongoose.model("Blog", blogSchema);
 
 // Restful Routes
-
 app.get("/", function(req, res){
   res.redirect("/blogs");
 });
 
+// INDEX Route
 app.get("/blogs", function(req, res){
   Blog.find({}, function(err, blogs){
     if(err){
       console.log("ERROR!");
     } else {
       res.render("index", {blogs: blogs});
+    }
+  });
+});
+
+// NEW Route
+app.get("/blogs/new", function(req, res){
+  res.render("new");
+});
+
+// CREATE Route
+app.post("/blogs", function(req, res){
+  // create blog
+  Blog.create(req.body.blog, function(req, res){
+    if(err){
+      res.render("new");
+    } else {
+      // then, redirect to the index
+      res.redirect("/blogs");
     }
   });
 });
