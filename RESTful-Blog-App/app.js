e516fd7056f3e2692b9ee9ec04a40b5a0f4a9b50
James Le <le_j6@denison.edu>
2017-10-25 16:44:01 +0700

    RESTful Blog App: Edit and Update

--- RESTful-Blog-App/app.js
+++ RESTful-Blog-App/app.js
@@ -1,4 +1,5 @@
 var bodyParser = require("body-parser"),
+    methodOverride = require("method-override"),
     mongoose = require("mongoose"),
     express = require("express"),
     app = express();
@@ -8,6 +9,7 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // Mongoose / Model Config
 var blogSchema = new mongoose.Schema({
@@ -59,6 +61,28 @@ app.get("/blogs/:id", function(req, res){
       res.redirect("/blogs");
     } else {
       res.render("show", {blog: foundBlog});
+    }
+  });
+});
+
+// EDIT Route
+app.get("/blogs/:id/edit", function(req, res){
+  Blog.findById(req.params.id, function(err, foundBlog){
+    if(err){
+      res.redirect("/blogs");
+    } else {
+      res.render("edit", {blog: foundBlog});
+    }
+  });
+});
+
+// UPDATE Route
+app.post("/blog/:id", function(req, res){
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+    if(err){
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs/" + req.params.id);
     }
   });
 });
