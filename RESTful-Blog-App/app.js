d698162957eb5727f5344576189b53851e1fea39
James Le <le_j6@denison.edu>
2017-10-25 16:26:38 +0700

    RESTful Blog App: Show

--- RESTful-Blog-App/app.js
+++ RESTful-Blog-App/app.js
@@ -52,6 +52,17 @@ app.post("/blogs", function(req, res){
   });
 });
 
+// SHOW Route
+app.get("/blogs/:id", function(req, res){
+  Blog.findById(req.params.id, function(err, foundBlog){
+    if(err){
+      res.redirect("/blogs");
+    } else {
+      res.render("show", {blog: foundBlog});
+    }
+  });
+});
+
 app.listen(process.env.PORT || 4000, process.env.IP, function(){
   console.log("Server is running");
 })
